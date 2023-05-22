15eba260f108c331a898da7b615e142c25ea3e91
s4mp41xao <samribe@outlook.com>
2023-05-21 21:36:51 -0300

    comments update

--- aulas/etapa-01/aula-03-02/app.js
+++ aulas/etapa-01/aula-03-02/app.js
@@ -6,9 +6,13 @@ const randomArray = ['Parker', 'Diana', 19, 18]
 
 // Métodos de arrays
 const joinHeroes = heroes.join(' | ')
+// Retorna o index de um item dentro do array.
 const indexOf25 = ages.indexOf(25)
+// Concatena novos items dentro de um array, sem modificar o array original.
 const moreHeroes = heroes.concat(['Superman', 'Wolverine'])
+// Insere items no array original 
 const pushToHeroes = heroes.push('Cyclops', 'Hulk') // 'console.log(pushToHeroes)' // 5
+// Remove e retornar o último item de um array.
 const popHeroes = heroes.pop()
 
 console.log(heroes) // 'Hulk'
