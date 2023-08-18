ded9a2d8447d15fbfaaa9ab49b671dc6f21cec45
s4mp41xao <samribe@outlook.com>
2023-08-18 14:46:15 -0300

    [etapa-05] modificando estilos css(inline)(aula-02-4)

---
+++ aulas/etapa-05 - DOM/aula-02-04 modificando estilos css (inline)/app.js
@@ -0,0 +1,18 @@
+// console.log(title)
+// title.setAttribute('style', 'margin: 50px;')
+
+
+
+const title = document.querySelector('h1')
+
+console.log(title.style)
+console.log(title.style.color)
+
+title.style.margin = '50px'
+title.style.color = 'red'
+
+
+
+title.style.fontSize = '50px'
+
+title.style.margin = ''
