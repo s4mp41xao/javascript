b84151a1e21d62fc4046d59a6f8c993d0ce3609e
s4mp41xao <samribe@outlook.com>
2023-09-14 11:40:01 -0300

    [etapa-08] método sort (aula-02-02)

---
+++ aulas/etapa-08 métodos de array/aula-02-02 sort/app.js
@@ -0,0 +1,15 @@
+const names = ['Christian', 'Alfredo', 'Edson']
+
+names.sort()
+
+const scores = [10, 50, 20, 5, 35, 70, 45]
+
+scores.sort((score1, score2) => score2 - score1)
+
+debugger
+
+const scores = [10, 50, 20, 5, 35, 70, 45]
+
+scores.sort((score1, score2) => score2 - score1)
+
+debugger
