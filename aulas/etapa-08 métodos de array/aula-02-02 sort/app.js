eada6c3c034d8931d4b0c7a834485143d7732051
s4mp41xao <samribe@outlook.com>
2023-09-14 11:55:42 -0300

    [etapa-08] encadeando métodos (aula-02-03)

--- aulas/etapa-08 métodos de array/aula-02-02 sort/app.js
+++ aulas/etapa-08 métodos de array/aula-02-02 sort/app.js
@@ -8,8 +8,15 @@ scores.sort((score1, score2) => score2 - score1)
 
 debugger
 
-const scores = [10, 50, 20, 5, 35, 70, 45]
+const theBigFamily = [
+  { name: 'Lineu', score: 20 },
+  { name: 'Nenê', score: 10 },
+  { name: 'Tuco', score: 50 },
+  { name: 'Bebel', score: 30 },
+  { name: 'Agostinho', score: 70 }
+]
 
-scores.sort((score1, score2) => score2 - score1)
+theBigFamily.sort((item1, item2) => item2.score - item1.score)
 
+theBigFamily
 debugger
